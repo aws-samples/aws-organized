6cb4d9876f1985c8d2330152f778c5f8aabe6fbd
Eamonn Faherty <eamonnf@amazon.co.uk>
2021-02-19 14:19:04 +0000

    implementing missing migrations

--- aws_organized/extensions/service_control_policies/migrations.py
+++ aws_organized/extensions/service_control_policies/migrations.py
@@ -1,5 +1,5 @@
 from typing import Callable, Tuple
-
+import json
 
 POLICY_ATTACH = "POLICY_ATTACH"
 POLICY_DETAILS_UPDATE = "POLICY_DETAILS_UPDATE"
@@ -12,25 +12,78 @@ MigrationResult = Tuple[bool, str]
 
 def policy_details_update(
     client,
+    id: str,
+    name: str,
+    description: str,
 ) -> MigrationResult:
+    try:
+        client.update_policy(
+            PolicyId=id,
+            Name=name,
+            Description=description,
+        )
+    except botocore.exceptions.ClientError as error:
+        message = error.response["Error"]["Message"]
+        return False, message
+    except:
+        message = sys.exc_info()[0]
+        return False, "{0}".format(message)
     return True, OK
 
 
-def policy_content_update(
-    client,
-) -> MigrationResult:
+def policy_content_update(client, id: str, content: str) -> MigrationResult:
+    try:
+        client.update_policy(
+            PolicyId=id,
+            Content=content,
+        )
+    except botocore.exceptions.ClientError as error:
+        message = error.response["Error"]["Message"]
+        return False, message
+    except:
+        message = sys.exc_info()[0]
+        return False, "{0}".format(message)
     return True, OK
 
 
 def policy_create(
     client,
+    name: str,
+    content: str,
 ) -> MigrationResult:
+    try:
+        client.create_policy(
+            Content=json.dumps(json.loads(content), indent=0),
+            Name=name,
+            Type="SERVICE_CONTROL_POLICY",
+        )
+
+    except botocore.exceptions.ClientError as error:
+        message = error.response["Error"]["Message"]
+        return False, message
+    except:
+        message = sys.exc_info()[0]
+        return False, "{0}".format(message)
     return True, OK
 
 
 def policy_attach(
     client,
+    policy_id: str,
+    target_id: str,
 ) -> MigrationResult:
+    try:
+        client.attach_policy(
+            PolicyId=policy_id,
+            TargetId=target_id,
+        )
+
+    except botocore.exceptions.ClientError as error:
+        message = error.response["Error"]["Message"]
+        return False, message
+    except:
+        message = sys.exc_info()[0]
+        return False, "{0}".format(message)
     return True, OK
 
 
